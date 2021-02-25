d072b2db9799691c9e482b6d38742019bafe74e4
ericaphillips <ericaphill@gmail.com>
2021-02-25 13:50:10 -0600

    6 done

--- chinook.session.sql
+++ chinook.session.sql
@@ -20,3 +20,12 @@ SELECT DISTINCT BillingCountry
 FROM Invoice
 ORDER BY BillingCountry ASC
 
+SELECT e.FirstName, e.LastName, InvoiceId 
+FROM Employee e
+JOIN Customer c 
+    ON e.EmployeeId = c.SupportRepId
+JOIN Invoice i 
+    ON c.CustomerId = i.CustomerId
+ORDER BY i.invoiceId ASC
+
+
