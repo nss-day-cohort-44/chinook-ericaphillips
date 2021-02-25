4e104f7504bd3d9b028910a150ee494e7d411662
ericaphillips <ericaphill@gmail.com>
2021-02-25 13:26:46 -0600

    3 done

--- chinook.session.sql
+++ chinook.session.sql
@@ -6,3 +6,8 @@ SELECT FirstName, LastName, CustomerId, Country
 FROM Customer
 WHERE Country = "Brazil"
 
+SELECT FirstName, LastName, InvoiceId, InvoiceDate, BillingCountry
+FROM Customer c
+JOIN Invoice i
+    ON c.CustomerId = i.CustomerId
+WHERE Country = "Brazil"
