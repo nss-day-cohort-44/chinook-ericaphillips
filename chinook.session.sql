6da6194746e5709c91ce2fedac54e26c5ad4c3fe
ericaphillips <ericaphill@gmail.com>
2021-02-26 13:33:51 -0600

    16 done

--- chinook.session.sql
+++ chinook.session.sql
@@ -78,4 +78,19 @@ ORDER BY InvoiceLineId ASC
 SELECT COUNT(BillingCountry), BillingCountry
 FROM Invoice
 GROUP BY BillingCountry
-   
+
+SELECT COUNT(TrackId), t.PlaylistId, Name
+FROM PlaylistTrack t
+JOIN Playlist p
+    ON p.PlaylistId = t.PlaylistId
+GROUP BY t.PlaylistId
+
+SELECT a.Title AS Album_Name, t.Name AS Track_Name, m.Name AS MediaType, g.Name AS Genre  
+FROM Track t
+JOIN Album a 
+    ON t.AlbumId = a.AlbumId
+JOIN Genre g
+    ON g.GenreId = t.GenreId
+JOIN MediaType m
+    ON m.MediaTypeId = t.MediaTypeId
+ORDER BY a.Title ASC
