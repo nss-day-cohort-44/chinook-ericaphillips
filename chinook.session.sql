bcad1eb4cb46afa6462d198f3f8734ab0e1d0423
ericaphillips <ericaphill@gmail.com>
2021-02-26 10:47:00 -0600

    9 done

--- chinook.session.sql
+++ chinook.session.sql
@@ -39,3 +39,15 @@ ORDER BY c.FirstName ASC
 SELECT COUNT (InvoiceDate)
 FROM Invoice i
 WHERE InvoiceDate LIKE '2009%' OR InvoiceDate LIKE '2011%' 
+
+SELECT SUM(total)
+FROM Invoice i
+WHERE InvoiceDate LIKE '2009%' OR InvoiceDate LIKE '2011%'
+GROUP BY InvoiceDate  LIKE "2009%"
+
+SELECT SUM(total),
+    strftime("%Y", InvoiceDate) as year
+FROM Invoice
+WHERE year == "2009" OR year == "2011"
+GROUP BY year;
+   
